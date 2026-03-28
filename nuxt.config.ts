82ab5f2f8f4e5add4b2feeb195467831f895f4a6
Radek Dobrovolný <radek.dobrovolny@gmail.com>
2026-03-28 18:35:14 +0100

    feature: Favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,12 @@ export default defineNuxtConfig({
   css: ["~~/assets/css/tailwind.css"],
   app: {
     head: {
-      viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
+      viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover",
+      link: [
+        { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+        { rel: "icon", type: "image/png", href: "/favicon.png" },
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" }
+      ]
     }
   },
   runtimeConfig: {
